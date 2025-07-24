0714461d68912bdaf27a43bbeebd323a0f55150f
ricky <kelleyricky30@gmail.com>
2025-07-23 22:54:41 -0700

    log the file to see if it absorbs {

--- export.js
+++ export.js
@@ -169,7 +169,6 @@ async function main() {
       process.exit(1);
     }
   }
-  console.log(TOKEN) // for debugging the gh action 
   if (!TOKEN && !DRY_RUN) {
     console.error('❌ No token specified.');
     process.exit(1);
